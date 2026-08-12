01f664c3f3623a1fbf8bd2f8577877671a5ad0e4
RUDRAPPASHANKAR8585 <150207928+RUDRAPPASHANKAR8585@users.noreply.github.com>
2026-08-12 09:19:48 +0530

    Add variables for AWS resource naming convention

--- ec2/Ctsdata.tf
+++ ec2/Ctsdata.tf
@@ -34,3 +34,66 @@ data "aws_security_group" "selected" {
     values = [data.aws_vpc.selected.id]
   }
 }
+
+
+
+
+variable "region_name" {
+  description = "Short region identifier used in the organization's AWS resource naming convention."
+  type        = string
+}
+
+variable "account_name" {
+  description = "Short account identifier used in the organization's AWS resource naming convention."
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name used in the resource naming convention."
+  type        = string
+}
+
+variable "vpc_suffix" {
+  description = "VPC naming suffix used by the organization's naming convention."
+  type        = string
+  default     = "R27"
+}
+
+variable "availability_zone_code" {
+  description = "Internal availability zone code used in the subnet name, for example A01, A02, or B01."
+  type        = string
+}
+
+# ------------------------------------------------------------
+# Existing VPC
+# ------------------------------------------------------------
+
+data "aws_vpc" "selected" {
+  filter {
+    name = "tag:Name"
+
+    values = [
+      "VPC${var.region_name}R${var.account_name}${var.vpc_suffix}${var.environment}"
+    ]
+  }
+}
+
+
+# ------------------------------------------------------------
+# Existing Subnet
+# ------------------------------------------------------------
+
+data "aws_subnet" "selected" {
+  filter {
+    name = "tag:Name"
+
+    values = [
+      "PSR${var.account_name}-${var.region_name}${var.availability_zone_code}"
+    ]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+}
