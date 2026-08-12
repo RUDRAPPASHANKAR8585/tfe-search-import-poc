22aa2811df4500a52d668b245a5d324474c2e5bb
RUDRAPPASHANKAR8585 <150207928+RUDRAPPASHANKAR8585@users.noreply.github.com>
2026-08-12 20:36:05 +0530

    Add import statements for RDS cluster and instance

---
+++ rds/ctsimport.tf
@@ -0,0 +1,9 @@
+import {
+  to = module.rds.aws_rds_cluster.this
+  id = "tfefdo-aurorapgsql-dr-testing"
+}
+
+import {
+  to = module.rds.aws_rds_cluster_instance.writer
+  id = "tfefdo-aurorapgsql-dr-testing-0"
+}
