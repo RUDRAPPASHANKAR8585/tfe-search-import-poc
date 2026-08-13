7ff5d6185aee2326686a7e12acb5deb39dce2e51
RUDRAPPASHANKAR8585 <150207928+RUDRAPPASHANKAR8585@users.noreply.github.com>
2026-08-13 20:26:41 +0530

    Update S3 import configurations for modules

--- s3/import.tf
+++ s3/import.tf
@@ -1,4 +1,50 @@
 import {
-  to = aws_s3_bucket.unmamaged
-  id = "shankar-poc-11"
+  to = module.s3["unmanaged1"].aws_s3_bucket.this
+  id = "tfe-search-import-1-poc"
+}
+
+import {
+  to = module.s3["unmanaged1"].aws_s3_bucket_ownership_controls.this
+  id = "tfe-search-import-1-poc"
+}
+
+import {
+  to = module.s3["unmanaged1"].aws_s3_bucket_public_access_block.this
+  id = "tfe-search-import-1-poc"
+}
+
+import {
+  to = module.s3["unmanaged1"].aws_s3_bucket_server_side_encryption_configuration.this
+  id = "tfe-search-import-1-poc"
+}
+
+import {
+  to = module.s3["unmanaged1"].aws_s3_bucket_versioning.this
+  id = "tfe-search-import-1-poc"
+}
+
+
+import {
+  to = module.s3["unmanaged2"].aws_s3_bucket.this
+  id = "tfe-search-import-2-poc"
+}
+
+import {
+  to = module.s3["unmanaged2"].aws_s3_bucket_ownership_controls.this
+  id = "tfe-search-import-2-poc"
+}
+
+import {
+  to = module.s3["unmanaged2"].aws_s3_bucket_public_access_block.this
+  id = "tfe-search-import-2-poc"
+}
+
+import {
+  to = module.s3["unmanaged2"].aws_s3_bucket_server_side_encryption_configuration.this
+  id = "tfe-search-import-2-poc"
+}
+
+import {
+  to = module.s3["unmanaged2"].aws_s3_bucket_versioning.this
+  id = "tfe-search-import-2-poc"
 }
